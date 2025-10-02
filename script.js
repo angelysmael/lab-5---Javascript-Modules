6c9bdaec55a4fc651928fc1e36642b9b2b91d619
Angel Ysmael <angelysmael7@gmail.com>
2025-10-02 18:19:22 -0400

    Initial commit: basic HTML/CSS and background

---
+++ script.js
@@ -0,0 +1,40 @@
+let studentName = "Angel";
+let studentAge = 19;
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+let exampleString = "Hello, World!";
+let exampleNumber = 123;
+let exampleBoolean = false;
+let exampleArrary = [1, 2, 3, 4, 5];
+let exampleObject = { firstName: "Angel",
+    lastName: "Ysmael"
+};
+    console.log("String:", exampleString);
+    console.log("Number:", exampleNumber);
+    console.log("Boolean:", exampleBoolean);
+    console.log("Array:", exampleArrary);
+    console.log("Object:", exampleObject);
+
+let a = 10;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+console.log("Greater than:", a > b);
+console.log("Less than:", a < b);
+console.log("Equal (strict):", a === b);
+console.log("Not equal:", a !== b);
+
+let x = true;
+let y = false;
+
+console.log("AND:", x && y);
+console.log("OR:", x || y);
+console.log("NOT x:", !x);
