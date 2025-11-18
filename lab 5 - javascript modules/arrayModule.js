66ba7282e72e7f8a49d22ada21b3aee61601484b
Angel Ysmael <angelysmael7@gmail.com>
2025-11-17 19:28:31 -0500

    final upload

---
+++ lab 5 - javascript modules/arrayModule.js
@@ -0,0 +1,10 @@
+export function findMax(arr) {
+    return Math.max(...arr);
+}
+
+export function findMin(arr) {
+    return Math.min(...arr);
+}
+export function reverseArray(arr){
+    return [...arr].reverse();
+}
